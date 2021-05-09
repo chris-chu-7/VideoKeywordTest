49eb7dcc356ffcc2a9ea643ce53d065d55693a62
Kevin Mui <kmui2@wisc.edu>
2021-05-09 03:36:43 -0500

    Use speech to text api

--- server/main.py
+++ server/main.py
@@ -1,7 +1,9 @@
+import json
 import logging
 import os
 from pathlib import Path
 import uvicorn
+import subprocess
 
 from fastapi import FastAPI
 from fastapi.logger import logger
@@ -11,6 +13,24 @@ from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 from starlette.requests import Request
 from starlette.responses import Response
+from converter import Converter
+from google.cloud import speech
+from google.cloud import storage
+from pytube import YouTube
+from google.protobuf.json_format import MessageToDict, MessageToJson
+
+dirname = Path(__file__).resolve().parent
+
+credential_path = dirname / "credentials/VideoKeywordTest-8e1da767906e.json"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = str(credential_path.absolute())
+
+audio_folder_path = dirname / "audio"
+audio_folder_path.mkdir(exist_ok=True)
+transcripts_folder_path = dirname / "transcripts"
+transcripts_folder_path.mkdir(exist_ok=True)
+audio_converter = Converter()
+speech_client = speech.SpeechClient()
+storage_client = storage.Client.from_service_account_json(credential_path)
 
 logging.basicConfig(
     level=logging.INFO,
@@ -71,85 +91,62 @@ class GetTranscriptResBody(BaseModel):
 
 @app.get("/transcript", response_model=GetTranscriptResBody)
 async def get_transcript(url: str):
-    return GetTranscriptResBody(
-        words=[
-            Word(**{"startTime": "0s", "endTime": "0.900s", "word": "in"}),
-            Word(**{"startTime": "0.900s", "endTime": "1.400s", "word": "December"}),
-            Word(**{"startTime": "1.400s", "endTime": "2.400s", "word": "2019"}),
-            Word(**{"startTime": "2.400s", "endTime": "3.400s", "word": "China"}),
-            Word(**{"startTime": "3.400s", "endTime": "3.900s", "word": "notified"}),
-            Word(**{"startTime": "3.900s", "endTime": "4s", "word": "the"}),
-            Word(**{"startTime": "4s", "endTime": "4.300s", "word": "World"}),
-            Word(**{"startTime": "4.300s", "endTime": "4.600s", "word": "Health"}),
-            Word(
-                **{"startTime": "4.600s", "endTime": "5.500s", "word": "Organization"}
-            ),
-            Word(**{"startTime": "5.500s", "endTime": "5.700s", "word": "but"}),
-            Word(**{"startTime": "5.700s", "endTime": "6.100s", "word": "several"}),
-            Word(**{"startTime": "6.100s", "endTime": "6.500s", "word": "cases"}),
-            Word(**{"startTime": "6.500s", "endTime": "6.700s", "word": "of"}),
-            Word(**{"startTime": "6.700s", "endTime": "6.900s", "word": "human"}),
-            Word(**{"startTime": "6.900s", "endTime": "7.400s", "word": "respiratory"}),
-            Word(**{"startTime": "7.400s", "endTime": "8.100s", "word": "illness"}),
-            Word(**{"startTime": "8.100s", "endTime": "8.500s", "word": "a"}),
-            Word(**{"startTime": "8.500s", "endTime": "8.600s", "word": "disease"}),
-            Word(**{"startTime": "8.600s", "endTime": "9.200s", "word": "later"}),
-            Word(**{"startTime": "9.200s", "endTime": "9.600s", "word": "named"}),
-            Word(**{"startTime": "9.600s", "endTime": "10.700s", "word": "covid-19"}),
-            Word(**{"startTime": "10.700s", "endTime": "11.400s", "word": "the"}),
-            Word(**{"startTime": "11.400s", "endTime": "11.900s", "word": "virus"}),
-            Word(**{"startTime": "11.900s", "endTime": "12.500s", "word": "using"}),
-            Word(**{"startTime": "12.500s", "endTime": "12.600s", "word": "this"}),
-            Word(**{"startTime": "12.600s", "endTime": "13.100s", "word": "disease"}),
-            Word(**{"startTime": "13.100s", "endTime": "13.300s", "word": "is"}),
-            Word(**{"startTime": "13.300s", "endTime": "13.700s", "word": "known"}),
-            Word(**{"startTime": "13.700s", "endTime": "13.800s", "word": "as"}),
-            Word(**{"startTime": "13.800s", "endTime": "14.400s", "word": "severe"}),
-            Word(**{"startTime": "14.400s", "endTime": "14.600s", "word": "acute"}),
-            Word(
-                **{"startTime": "14.600s", "endTime": "14.900s", "word": "respiratory"}
-            ),
-            Word(**{"startTime": "14.900s", "endTime": "15.900s", "word": "syndrome"}),
-            Word(
-                **{"startTime": "15.900s", "endTime": "16.200s", "word": "coronavirus"}
-            ),
-            Word(**{"startTime": "16.200s", "endTime": "17.200s", "word": "to"}),
-            Word(**{"startTime": "17.200s", "endTime": "18.900s", "word": "the"}),
-            Word(**{"startTime": "18.900s", "endTime": "19.100s", "word": "disease"}),
-            Word(**{"startTime": "19.100s", "endTime": "19.400s", "word": "spreads"}),
-            Word(**{"startTime": "19.400s", "endTime": "20.200s", "word": "through"}),
-            Word(**{"startTime": "20.200s", "endTime": "20.400s", "word": "small"}),
-            Word(**{"startTime": "20.400s", "endTime": "20.800s", "word": "droplets"}),
-            Word(**{"startTime": "20.800s", "endTime": "21.100s", "word": "that"}),
-            Word(**{"startTime": "21.100s", "endTime": "21.400s", "word": "are"}),
-            Word(**{"startTime": "21.400s", "endTime": "21.800s", "word": "expelled"}),
-            Word(**{"startTime": "21.800s", "endTime": "21.900s", "word": "from"}),
-            Word(**{"startTime": "21.900s", "endTime": "22.100s", "word": "the"}),
-            Word(**{"startTime": "22.100s", "endTime": "22.300s", "word": "nose"}),
-            Word(**{"startTime": "22.300s", "endTime": "22.400s", "word": "or"}),
-            Word(**{"startTime": "22.400s", "endTime": "22.500s", "word": "mouth"}),
-            Word(**{"startTime": "22.500s", "endTime": "23.100s", "word": "when"}),
-            Word(**{"startTime": "23.100s", "endTime": "23.100s", "word": "a"}),
-            Word(**{"startTime": "23.100s", "endTime": "23.500s", "word": "person"}),
-            Word(**{"startTime": "23.500s", "endTime": "23.600s", "word": "with"}),
-            Word(**{"startTime": "23.600s", "endTime": "24s", "word": "covid-19"}),
-            Word(**{"startTime": "24s", "endTime": "24.900s", "word": "cough"}),
-            Word(**{"startTime": "24.900s", "endTime": "25.300s", "word": "or"}),
-            Word(**{"startTime": "25.300s", "endTime": "26s", "word": "exhales"}),
-            Word(**{"startTime": "26s", "endTime": "28.100s", "word": "that."}),
-            Word(**{"startTime": "28.100s", "endTime": "28.400s", "word": "Even"}),
-            Word(**{"startTime": "28.400s", "endTime": "28.800s", "word": "close"}),
-            Word(**{"startTime": "28.800s", "endTime": "29s", "word": "to"}),
-            Word(**{"startTime": "29s", "endTime": "29.200s", "word": "someone"}),
-            Word(**{"startTime": "29.200s", "endTime": "29.600s", "word": "who's"}),
-            Word(**{"startTime": "29.600s", "endTime": "29.700s", "word": "infected"}),
-            Word(**{"startTime": "29.700s", "endTime": "30.500s", "word": "can"}),
-            Word(**{"startTime": "30.500s", "endTime": "30.700s", "word": "put"}),
-            Word(**{"startTime": "30.700s", "endTime": "30.800s", "word": "you"}),
-            Word(**{"startTime": "30.800s", "endTime": "30.900s", "word": "at"}),
-            Word(**{"startTime": "30.900s", "endTime": "31.100s", "word": "risk"}),
-        ]
-    )
+
+    # Download Audio of the YouTube video
+    yt = YouTube(url)
+    mp4_audio_file_path = audio_folder_path / f"{yt.video_id}.mp4"
+    wav_audio_file_path = audio_folder_path / f"{yt.video_id}.wav"
+    transcript_file_path = transcripts_folder_path / f"{yt.video_id}.json"
+
+    full_words_list = []
+
+    if transcript_file_path.exists():
+        full_words_list = json.loads(transcript_file_path.read_text())
+    else:
+        stream = yt.streams.filter(only_audio=True, subtype="mp4").first()
+        stream.download(audio_folder_path, filename=yt.video_id)
+
+        # Convert video to WAV format
+        command = f"ffmpeg -y -i {mp4_audio_file_path} -ab 160k -ac 2 -ar 16000 -vn {wav_audio_file_path}"
+        subprocess.call(command, shell=True)
+
+        # Upload video to Google Cloud Storage
+        bucket = storage_client.get_bucket("video-keyword-test")
+        blob = bucket.blob(f"{yt.video_id}.wav")
+        blob.upload_from_filename(filename=str(wav_audio_file_path.absolute()))
+
+        # Run the Google Speech-to-Text API
+        audio = speech.RecognitionAudio(
+            uri=f"gs://video-keyword-test/{yt.video_id}.wav"
+        )
+        config = speech.RecognitionConfig(
+            sample_rate_hertz=16000,
+            language_code="en-US",
+            audio_channel_count=2,
+            enable_word_time_offsets=True,
+        )
+        operation = speech_client.long_running_recognize(config=config, audio=audio)
+        response = operation.result()
+        if len(response.results) == 0:
+            raise Exception("Empty result")
+
+        # Download the Google Speech-to-Text results
+        # Each result is for a consecutive portion of the audio. Iterate through
+        # them to get the transcripts for the entire audio file.
+        for i, result in enumerate(response.results):
+            # The first alternative is the most likely one for this portion.
+            print("Transcript: {}".format(result.alternatives[0].transcript))
+            print("Confidence: {}".format(result.alternatives[0].confidence))
+
+            most_probable_result = result.alternatives[0]
+
+            dict_result = MessageToDict(most_probable_result._pb)
+            full_words_list.extend(dict_result["words"])
+
+        with open(transcript_file_path, "w") as json_file:
+            json_file.write(json.dumps(full_words_list))
+
+    return GetTranscriptResBody(words=[Word(**word) for word in full_words_list])
 
 
 build_dir_path = dirname / ".." / "web-app" / "build"
